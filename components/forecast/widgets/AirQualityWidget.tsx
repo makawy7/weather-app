b61a39f3e1bdd995b7eac0d7742821023069e048
makawy7 <abdallah@abdallahs-MacBook-Pro.local>
2024-05-17 22:03:55 +0300

    air quality widget

--- components/forecast/widgets/AirQualityWidget.tsx
+++ components/forecast/widgets/AirQualityWidget.tsx
@@ -1,6 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import Widget, { WidgetDimensionProps } from './base/Widget'
+import { AntDesign, Entypo } from '@expo/vector-icons'
+import {
+  Canvas,
+  Circle,
+  Line,
+  LinearGradient,
+  vec,
+} from '@shopify/react-native-skia'
 
 const AirQualityWidget: React.FC<WidgetDimensionProps> = ({
   width,
@@ -8,7 +16,49 @@ const AirQualityWidget: React.FC<WidgetDimensionProps> = ({
 }) => {
   return (
     <Widget width={width} height={height}>
-      <Text style={{ color: 'white' }}>Air Quality</Text>
+      <Widget.Header
+        name="AIR QUALITY"
+        Icon={Entypo}
+        iconProps={{ name: 'air' }}
+      />
+      <Widget.Body text="3-Low Health Risk">
+        <Canvas
+          style={{
+            height: 15,
+            marginTop: 8,
+          }}
+        >
+          <Line
+            p1={vec(0, 5)}
+            p2={vec(width, 5)}
+            strokeWidth={5}
+            style="stroke"
+          >
+            <LinearGradient
+              start={vec(0, 0)}
+              end={vec(width, 0)}
+              colors={['#3859B2', '#E64497']}
+            />
+          </Line>
+          <Circle
+            cx={0}
+            cy={5}
+            r={5}
+            color="white"
+            transform={[{ translateX: 20 }]}
+          />
+        </Canvas>
+      </Widget.Body>
+      <Widget.Footer
+        text="See more"
+        Icon={AntDesign}
+        iconProps={{ name: 'right' }}
+        extraStyle={{
+          borderTopWidth: 0.5,
+          borderTopColor: 'rgba(173, 139, 139, 0.2)',
+          paddingTop: 10,
+        }}
+      />
     </Widget>
   )
 }
--- components/forecast/widgets/base/Widget.tsx
+++ components/forecast/widgets/base/Widget.tsx
@@ -6,7 +6,7 @@ import {
   ViewProps,
   ViewStyle,
 } from 'react-native'
-import React from 'react'
+import React, { ElementType } from 'react'
 
 export type WidgetDimensionProps = {
   width: number
@@ -20,6 +20,12 @@ type WidgetProps = {
   height: number
 } & ViewProps
 
+type IconProps = {
+  name: string
+  color?: string
+  size?: number
+}
+
 const Widget = ({ children, style, width, height }: WidgetProps) => {
   return (
     <View
@@ -32,6 +38,7 @@ const Widget = ({ children, style, width, height }: WidgetProps) => {
           borderColor: 'white',
           padding: 15,
           backgroundColor: '#201C48',
+          justifyContent: 'space-between',
         },
         style,
       ]}
@@ -41,10 +48,93 @@ const Widget = ({ children, style, width, height }: WidgetProps) => {
   )
 }
 
-Widget.Header = () => {}
-Widget.Body = () => {}
-Widget.Footer = () => {}
+type WidgetHeaderProps = {
+  Icon: ElementType<any>
+  name: string
+  iconProps: IconProps
+}
+
+Widget.Header = ({ Icon, name, iconProps }: WidgetHeaderProps) => {
+  return (
+    <View style={styles.flexRowBase}>
+      <Icon
+        color={iconProps.color || 'rgba(235,235,245,0.6)'}
+        name={iconProps.name}
+        size={iconProps.size || 20}
+      />
+      <Text style={styles.title}>{name}</Text>
+    </View>
+  )
+}
+
+type WidgetBodyProps = {
+  text: string
+  children: React.ReactNode
+}
+
+Widget.Body = ({ text, children }: WidgetBodyProps) => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text style={styles.regularText}>{text}</Text>
+      {children}
+    </View>
+  )
+}
+
+type WidgetFooterProps = {
+  text: string
+  Icon: ElementType<any>
+  iconProps: IconProps
+  extraStyle?: StyleProp<ViewStyle>
+}
+
+Widget.Footer = ({ text, Icon, iconProps, extraStyle }: WidgetFooterProps) => {
+  return (
+    <View style={[styles.flexRowBase, styles.justifyBetween, extraStyle]}>
+      <Text style={styles.footerText}>{text}</Text>
+      <Icon
+        color={iconProps.color || 'rgba(235,235,245,0.6)'}
+        name={iconProps.name}
+        size={iconProps.size || 20}
+      />
+    </View>
+  )
+}
 
 export default Widget
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  flexRowBase: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  justifyBetween: {
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontFamily: 'SF-Semibold',
+    fontSize: 16,
+    lineHeight: 16,
+    color: 'rgba(235, 235, 245, 0.6)',
+    paddingLeft: 6,
+  },
+  regularText: {
+    fontFamily: 'SF-Regular',
+    fontSize: 19,
+    lineHeight: 19,
+    color: 'white',
+  },
+  footerText: {
+    fontFamily: 'SF-Regular',
+    fontSize: 18,
+    lineHeight: 18,
+    color: 'white',
+  },
+  largeText: {
+    fontFamily: 'SF-Semibold',
+    color: 'white',
+    fontSize: 34,
+    lineHeight: 34,
+  },
+})
